a521d34d5cea8d758bff9bb027cb3c7669e344c0
sergiorvs <segio.rolando@gmail.com>
2017-04-02 23:56:29 -0500

    reinas funcionando perfecto

--- 8 Reinas/Reinas.cpp
+++ 8 Reinas/Reinas.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 using namespace std;
-#define tam 4
+#define tam 8
 
 class Tablero{
   public:
@@ -21,7 +21,6 @@ class Tablero{
         reinas=r;
     }
     
-    ///copia la matris anterior e inserta nueva ficha
     void siguiente_jugada(int matrix[tam][tam],int fil,int col)
     {
       for(int i=0;i<tam;i++)
@@ -30,22 +29,17 @@ class Tablero{
         {
           mat[i][j]=matrix[i][j];
         }
-      }
-      
+      } 
       insertar(fil,col);
     }
     void insertar(int fil,int col)
     {
         int af=fil;int ac=col;
-        for(int i=0;i<tam;i++)
-        {
-            mat[i][col]=-1;
-            mat[fil][i]=-1;
-            
-        }
-        
+
+        //fila/columna
+        for(int i=0;i<tam;i++){mat[i][col]=-1;mat[fil][i]=-1;}
+
         //diagonales
-        //for(int i=fil)
         while(af > 0 && ac > 0){af--;ac--; mat[af][ac]=-1;}ac=col;af=fil;
         while(af < tam-1 && ac < tam-1){af++;ac++; mat[af][ac]=-1;}ac=col;af=fil;
         while(af > 0 && ac < tam-1){af--;ac++; mat[af][ac]=-1;}ac=col;af=fil;
@@ -54,17 +48,6 @@ class Tablero{
         reinas++;
     }
     
-    bool analizarJugada()
-    {
-        for(int i=0;i<tam;i++)
-        {
-          for(int j=0;j<tam;j++)
-          {
-            if(mat[i][j]==0)return true;
-          }
-        }
-        return false;
-    }
     void print_tablero()
     {
         for(int i=0;i<tam;i++)
@@ -77,80 +60,104 @@ class Tablero{
         }
     }
 };
-vector<Tablero* >q;
+
 class Arbol{
     public:
         Tablero* m_root;
-        
-        //int filReyna, colReyna;
         Arbol(int i,int j)
         {
           m_root=NULL;
           m_root=new Tablero(0);
           m_root->insertar(i,j);
         }
-        
-        void insertar_reina(Tablero* aux)
+
+        Arbol()
         {
-            
-            for(int i=0;i<tam;i++)
-            {
-              for(int j=0;j<tam;j++)
-              {
-                if(aux->mat[i][j]==0)
-                {
-                  aux->m_child.push_back(new Tablero(aux->reinas));
-                  aux->m_child[aux->m_child.size()-1]->siguiente_jugada(aux->mat,i,j);
-                }
-              }
-            }
-            insert_all(aux);
-                    
-          
+        	m_root=NULL;
+        	m_root=new Tablero(0);
         }
         
-        void insert_all(Tablero* aux)
+        void insertar_reina()
         {
-            
-            if(aux->analizarJugada())
-            {
-             
-                for(int i=0;i< aux->m_child.size() ; i++)
-                {
-                    //cout<<"i: "<<i<<endl;
-                    //cout<<"hijos: "<<aux->m_child.size()<<endl;
-                    aux=aux->m_child[i];
-                    q.push_back(aux);
-                    //if(aux->m_child[i]) insertar_reina(aux->m_child[i]);
-                }
-            }
-            while(q.size()>0)
-            {
-                Tablero* tmp;
-                tmp=q[q.size()-1];
-                q.pop_back();
-                insertar_reina(tmp);
-            }        
+        	vector<Tablero* >jugadas;
+        	jugadas.push_back(m_root);
+        	Tablero* tmp;
+        	while(jugadas.size()>0)
+        	{
+        		tmp=jugadas[0];
+        		if(tmp->reinas==tam)break;
+        		jugadas.erase(jugadas.begin());
+        		for(int i=0;i<tam;i++)
+	            {
+	                if(tmp->mat[tmp->reinas][i]==0)
+	                {
+	                  tmp->m_child.push_back(new Tablero(tmp->reinas));
+	                  tmp->m_child[tmp->m_child.size()-1]->siguiente_jugada(tmp->mat,tmp->reinas,i);
+	                  jugadas.push_back(tmp->m_child[tmp->m_child.size()-1]);
+	                }
+	            }
+        	}
+        }
 
-             
+        //recorrido en amplitud
+        void Soluciones(vector<Tablero* > &sols)
+        {
+        	vector<Tablero* >aux;
+        	aux.push_back(m_root);
+        	Tablero* tmp;
+        	while(aux.size()>0)
+        	{
+	        	tmp=aux[0];
+	        	aux.erase(aux.begin());
+	        	if(tmp->reinas==tam)sols.push_back(tmp);
+	        	for(int i=0;i<tmp->m_child.size();i++)
+	        	{
+	        		aux.push_back(tmp->m_child[i]);
+	        	}	
+        	}        	       	
         }
 };
+
+void imprimir_soluciones(vector<Tablero*> soluciones)
+{
+	char c;
+	for(int s=0;s<soluciones.size();s++)
+	{
+		cout<<s+1<<") ";
+		for(int i=0;i<tam;i++)
+		{
+			for(int j=0;j<tam;j++)
+			{
+				if(soluciones[s]->mat[i][j]==1)
+				{
+					c=65+i;
+					cout<<c<<j+1<<", ";
+				}
+			}
+		}
+		cout<<endl;
+	}
+}
+
 int main()
 {
-    Arbol a(0,0);
+    //Arbol a(0,1);
+    Arbol a;
     
     a.m_root->print_tablero();
     cout<<"-------------------------------------------------\n"<<endl;
-    a.insertar_reina(a.m_root);
-    a.insert_all(a.m_root);
-    cout<<a.m_root->m_child.size()<<endl;
-    cout<<a.m_root->m_child[1]->m_child.size()<<endl;
-    a.m_root->m_child[1]->print_tablero();
-    cout<<"-------------------------------------------------\n"<<endl;
-    a.m_root->m_child[1]->m_child[0]->print_tablero();
-    //a.m_root->m_child[0]->m_child[0]->print_tablero();
-    //cout<<"reinas puestas: "<<a.m_root->m_child[0]->m_child[0]->m_child[0]->m_child[0]->reinas<<endl;
-
+    a.insertar_reina();
+    vector<Tablero*>soluciones;
+    a.Soluciones(soluciones);
+    cout<<"EL NUMERO DE SOLUCIONES ES: "<<soluciones.size()<<endl;
+    /*for(int i=0;i<soluciones.size();i++)
+    {
+    	soluciones[i]->print_tablero();
+    	cout<<"-------------------------------------------------\n"<<endl;
+    }*/
+    //char c=65;
+    //cout<<c<<endl;
+    imprimir_soluciones(soluciones);
     return 0;
 }
 
