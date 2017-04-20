c92525d819fd8e5f6ea9c6139bec3daec4eea497
sergiorvs <segio.rolando@gmail.com>
2017-04-20 16:09:12 -0500

    funcionagit add Damas2das/git add Damas2das/git add Damas2das/git add Damas2das/git add Damas2das/git add Damas2das/git add Damas2das/git add Damas2das/git add Damas2das/

---
+++ Damas2das/Tablero.h
@@ -0,0 +1,200 @@
+#include "Funciones.h"
+#ifndef IADAMASMINMAC_TABLERO_H
+#define IADAMASMINMAC_TABLERO_H
+
+class Tablero
+{
+public:
+    vector<Tablero*>mChild;
+    int** mTablero;
+    bool mTurno;
+    int mNivelMinMax;
+    pair< pair<int,int>, vector< pair<int,int> > >  mJugadasPorFicha;
+    vector<int**> mTablerosUpdate;
+
+
+    Tablero(int** Matriz,bool turno,int nivelMinMax)
+    {
+        mTablero= new int*[tam];
+        for(int i=0;i<tam;i++)
+        {
+            mTablero[i]=new int[tam];
+        }
+        igualarMatrices(mTablero,Matriz);
+        mTurno = turno;
+        mNivelMinMax = nivelMinMax;
+
+    }
+
+
+
+    /*Reglas de las damas Parámetro Ficha a Evaluar movimientos*/
+    bool movimientosFicha(int i,int j)
+    {
+        vector< pair<int,int> > movimientosFicha; /*Movimientos que puede Realizar c/ficha*/
+        int comio = 0;
+        int flag  = 0;
+
+        if(mTurno == 0) /*Fichas Rojas :  Valor en la Matriz -1*/
+        {
+            /*Si la ficha puede Comer, el algoritmo fuerza a realizar esta jugada*/
+            if( ( i+1<tam && j+1<tam && mTablero[i+1][j+1] ==1 ))
+            {
+                if( ( i+2<tam && j+2<tam && mTablero[i+2][j+2] == 0 ) )
+                {
+                    movimientosFicha.push_back(make_pair(i+2,j+2));
+                    flag++; comio++;
+                }
+            }
+            if( i+1<tam && j-1>=0 && mTablero[i+1][j-1] == 1 )
+            {
+                if( i+2<tam && j-2>=0 && mTablero[i+2][j-2] == 0 )
+                {
+                    movimientosFicha.push_back(make_pair(i+2,j-2));
+                    flag++; comio++;
+
+                }
+            }
+            if(comio == 0)
+            {
+                /*Si no comio aun podría moverse*/
+                if( ( i+1<tam && j+1<tam && mTablero[i+1][j+1] == 0 ))
+                {
+                    movimientosFicha.push_back(make_pair(i+1,j+1));
+                    flag++;
+                }
+                if( ( i+1<tam && j-1>=0 && mTablero[i+1][j-1] == 0 ))
+                {
+                    movimientosFicha.push_back(make_pair(i+1,j-1));
+                    flag++;
+                }
+            }
+        }
+
+        else /*Fichas Negras Valor en la Matriz 1*/
+        {
+            if( ( i-1>=0 && j-1>=0 && mTablero[i-1][j-1] == -1 ))
+            {
+                if( ( i-2>=0 && j-2>=0 && mTablero[i-2][j-2] == 0 ) )
+                {
+                    movimientosFicha.push_back(make_pair(i-2,j-2));
+                    flag++; comio++;
+                }
+            }
+
+            if( ( i-1>=0 && j+1<tam && mTablero[i-1][j+1] == -1 ))
+            {
+                if( ( i-2>=0 && j+2<tam && mTablero[i-2][j+2] == 0 ) )
+                {
+                    movimientosFicha.push_back(make_pair(i-2,j+2));
+                    flag++; comio++;
+                }
+            }
+
+            if(comio == 0)
+            {
+                if( ( i-1>=0 && j-1>=0 && mTablero[i-1][j-1] == 0 ))
+                {
+                    movimientosFicha.push_back(make_pair(i-1,j-1));
+                    flag++;
+                }
+                if( ( i-1>=0 && j+1<tam && mTablero[i-1][j+1] == 0 ))
+                {
+                    movimientosFicha.push_back(make_pair(i-1,j+1));
+                    flag++;
+                }
+            }
+
+        }
+
+        if(flag!=0)
+        {
+            mJugadasPorFicha = make_pair(make_pair(i,j),movimientosFicha);
+            return 1;
+            /*cout<<"Los mov de la ficha "<<i<<" , "<<j<<"son: "<<endl;
+            for(int i=0;i<movimientosFicha.size();i++)
+            {
+                cout<<movimientosFicha[i].first<<" , "<<movimientosFicha[i].second<<endl;
+            }*/
+
+        }
+        return 0;
+    }
+
+    /*Ya con los movimientos de c/ficha queda realizar las N jugadas posibles y actualizar el tablero*/
+    void realizarJugada()
+    {
+        /*Si es el turno de las fichas Rojas*/
+        if(mTurno == 0)
+        {
+            for(int i=0; i<tam;i++)
+            {
+                for(int j=0;j<tam;j++)
+                {
+                    if(mTablero[i][j] == -1) /*Si es una ficha Roja Analiza posibles Mov por c/ficha*/
+                    {
+                        if(movimientosFicha(i,j)== 1)
+                            updateTablero(i,j,-1);
+                    }
+                }
+            }
+
+        }
+        else
+        {
+            for(int i=0; i<tam;i++)
+            {
+                for(int j=0;j<tam;j++)
+                {
+                    if(mTablero[i][j] == 1) /*Si es una ficha Negra Analiza posibles Mov por c/ficha*/
+                    {
+                        if(movimientosFicha(i,j)== 1)
+                            updateTablero(i,j,1);
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    void updateTablero(int fichaPosi, int fichaPosj,int value)
+    {
+        int posFinali,posFinalj;
+        for(int i=0;i<mJugadasPorFicha.second.size();i++)
+        {
+            int** matriz;
+            matriz= new int*[tam];
+            for(int a=0;a<tam;a++)
+            {
+                matriz[a]=new int[tam];
+            }
+            igualarMatrices(matriz,mTablero);
+            posFinali = mJugadasPorFicha.second[i].first;
+            posFinalj = mJugadasPorFicha.second[i].second;
+
+            /*Si el Mov es comer Pintamos la Tabla*/
+            if(abs(posFinali-fichaPosi)== 2)
+            {
+                if(posFinali-fichaPosi>0 && posFinalj-fichaPosj>0)matriz[fichaPosi+1][fichaPosj+1]=0;
+                if(posFinali-fichaPosi>0 && posFinalj-fichaPosj<0)matriz[fichaPosi+1][fichaPosj-1]=0;
+                if(posFinali-fichaPosi<0 && posFinalj-fichaPosj<0)matriz[fichaPosi-1][fichaPosj-1]=0;
+                if(posFinali-fichaPosi<0 && posFinalj-fichaPosj>0)matriz[fichaPosi-1][fichaPosj+1]=0;
+            }
+            matriz[fichaPosi][fichaPosj]=0;
+            matriz[posFinali][posFinalj]=value;
+            //printTablero(matriz);
+            mTablerosUpdate.push_back(matriz);
+        }
+
+    }
+
+};
+
+
+
+
+
+
+
+#endif //IADAMASMINMAC_TABLERO_H
---
+++ Damas2das/main.cpp
@@ -0,0 +1,136 @@
+#include "Tablero.h"
+class miniMax
+{
+public:
+    Tablero* mRoot;
+    int** mTablerito;
+    int mMaxLevel;
+    bool mTurno;
+    miniMax(int** matriz,bool turno,int level)
+    {
+        mTablerito= new int*[tam];
+        for(int i=0;i<tam;i++)
+        {
+            mTablerito[i]=new int[tam];
+        }
+        mRoot = NULL;
+        igualarMatrices(mTablerito,matriz);
+        mMaxLevel = level;
+        mTurno =  turno;
+        mRoot= new Tablero(mTablerito,mTurno,0);
+    }
+
+
+    void construirArbolNivelBase()
+    {
+        vector<Tablero* > next_jugadas;
+        Tablero *aux;
+        next_jugadas.push_back(mRoot);
+        while(next_jugadas.size()>0)
+        {
+            aux=next_jugadas[0];
+            //cout<<"maxlevel"<<mRoot->mNivelMinMax<<endl;
+            //cout<<"entro"<<endl;
+            if(aux->mNivelMinMax>mMaxLevel)break;
+            next_jugadas.erase(next_jugadas.begin());
+            aux->realizarJugada();
+            Tablero* ptr;
+            for(int i=0;i<aux->mTablerosUpdate.size();i++)
+            {
+                ptr=new Tablero(aux->mTablerosUpdate[i],!aux->mTurno,aux->mNivelMinMax+1);
+                aux->mChild.push_back(ptr);
+                next_jugadas.push_back(ptr);
+            }
+        }
+
+    }
+
+
+   /* void construirArbolNivelBase()
+    {
+        mRoot= new Tablero(mTablerito,mTurno,0);
+        mRoot->realizarJugada();
+        Tablero* ptr;
+        for(int i=0;i<mRoot->mTablerosUpdate.size();i++)
+        {
+            ptr = new Tablero(mRoot->mTablerosUpdate[i],!mTurno,mRoot->mNivelMinMax+1);
+            mRoot->mChild.push_back(ptr);
+            construirArbolNivelMaximo(mRoot);
+        }
+    }
+
+    void construirArbolNivelMaximo(Tablero* &ptr)
+    {
+        for(int i=0; i<ptr->mChild.size();i++)
+        {
+            if(ptr->mChild[i]->mNivelMinMax <= mMaxLevel)
+            {
+                ptr->mChild[i]->realizarJugada();
+
+                Tablero* aux;
+                for(int j=0;j<ptr->mChild[i]->mTablerosUpdate.size();j++)
+                {
+                    aux = new Tablero(ptr->mChild[i]->mTablerosUpdate[j],!(ptr->mChild[ei]->mTurno),ptr->mChild[i]->mNivelMinMax+1);
+                    ptr->mChild[i]->mChild.push_back(aux);
+                }
+                Tablero *ptr1 =ptr->mChild[i];
+                construirArbolNivelMaximo(ptr1);
+            }
+
+
+        }
+    }*/
+
+};
+int main() {
+
+    int** tabl;
+    tabl= new int*[tam];
+    for(int i=0;i<tam;i++)
+    {
+        tabl[i]=new int[tam];
+    }
+    //int tabl[tam][tam];
+    for(int i=0;i<tam;i++)
+    {
+        for(int j=0;j<tam;j++)
+        {
+            tabl[i][j]=0;
+        }
+    }
+    //----------negras--------
+    for(int i=0;i<tam;i+=2){tabl[0][i]=-1;}
+    for(int i=1;i<tam;i+=2){tabl[1][i]=-1;}
+    for(int i=0;i<tam;i+=2){tabl[2][i]=-1;}
+    //----------rojas--------
+    for(int i=1;i<tam;i+=2){tabl[7][i]=1;}
+    for(int i=0;i<tam;i+=2){tabl[6][i]=1;}
+    for(int i=1;i<tam;i+=2){tabl[5][i]=1;}
+
+    cout<<"Matriz Original: "<<endl;
+    /*cout<<"**********************************************************"<<endl;
+    for(int i=0;i<tam;i++)
+    {
+        for(int j=0;j<tam;j++)
+        {
+            cout<<"["<<tabl[i][j]<<" ]\t";
+        }
+        cout<<endl;
+    }
+    cout<<"**********************************************************"<<endl;*/
+    /*Tablero tablerito(tabl,0,0);
+    tablerito.realizarJugada();*/
+    miniMax arbol(tabl,0,5);
+    arbol.construirArbolNivelBase();
+    cout<<"Imprimiendo Hijos "<<endl;
+    /*for(int i=0;i<arbol.mRoot->mChild.size();i++)
+    {
+        cout<<"Turno "<<arbol.mRoot->mChild[i]->mTurno<<" Level: "<<arbol.mRoot->mChild[i]->mNivelMinMax<<endl;
+        printTablero(arbol.mRoot->mChild[i]->mTablero);
+    }*/
+    printTablero(arbol.mRoot->mTablero);
+    printTablero(arbol.mRoot->mChild[0]->mTablero);
+    printTablero(arbol.mRoot->mChild[0]->mChild[0]->mTablero);
+    printTablero(arbol.mRoot->mChild[0]->mChild[0]->mChild[0]->mTablero);
+    return 0;
+}
